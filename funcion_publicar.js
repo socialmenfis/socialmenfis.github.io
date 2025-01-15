7519032aa3852bc280a4250dd043e1916391800b
polaris <149647167+soluciones-polaris@users.noreply.github.com>
2025-01-15 10:05:40 -0500

    a

--- funcion_publicar.js
+++ funcion_publicar.js
@@ -9,6 +9,8 @@ function publicar_imagen(imageTitle, imageDescription)
     // "By posting, you agree to TikTok's Music Usage Confirmation"
 
     var contenido = `
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
             <style>
                 body {
                     font-family: Arial, sans-serif;
