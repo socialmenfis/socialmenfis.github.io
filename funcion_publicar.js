a48d367e988efec55026f13b6b69d82da3c75db4
polaris <149647167+soluciones-polaris@users.noreply.github.com>
2025-01-15 18:16:11 -0500

    a

--- funcion_publicar.js
+++ funcion_publicar.js
@@ -43,9 +43,9 @@ function publicar_imagen(imageTitle, imageDescription,nivel_privacyA)
     // Realiza la solicitud al endpoint
     const encodedCode = encodeURIComponent(localStorage.getItem("refresh_token"));
     const title = encodeURIComponent(imageTitle);
-    const description = encodeURIComponent(imageDescription.trim().length==0?"no_description":imageDescription.trim());
+    const disable_comment = encodeURIComponent(imageDescription);
     const nivel_privacy = encodeURIComponent(nivel_privacyA);  
-    fetch(`https://script.google.com/macros/s/AKfycbz5y5VUExFayCdNSVPVF-hw1ZKG6GZzez8EyrnH9V4kKgFI71EyIFQpdQQ5tzKeX-8J8Q/exec?refresh_token=${encodedCode}&title=${title}&description=${description}&nivel_privacy=${nivel_privacy}`)
+    fetch(`https://script.google.com/macros/s/AKfycbz5y5VUExFayCdNSVPVF-hw1ZKG6GZzez8EyrnH9V4kKgFI71EyIFQpdQQ5tzKeX-8J8Q/exec?refresh_token=${encodedCode}&title=${title}&disable_comment=${disable_comment}&nivel_privacy=${nivel_privacy}`)
     .then(response =>
     {
         if (!response.ok)
